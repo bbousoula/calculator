5624f04bb55f51b1a1829d357f7443d8406cd98b
Bradley Bousoula <bbousoul@proximitybbdo.fr>
2021-09-22 16:01:24 +0400

    refactor + fix calculation error

--- src/App.js
+++ src/App.js
@@ -11,13 +11,21 @@ const App = () => {
   const [result, setResult] = useState("");
 
   const addToText = (val) => {
-    setText((text) => [...text, val + " "]);
+    setText((text) => [...text, val + ""]);
   };
 
   const calculateResult = () => {
-    const input = text.join(""); //Remove commas
-    setResult(math.evaluate(input));
+    const input = text.join("");
+    try {
+      /* setResult(math.evaluate(input)); */
+      setResult(parse(input));
+    } catch (err) {
+      setResult("Error");
+      console.log(err);
+    }
   };
+
+  const parse = (str) => Function(`'use strict'; return (${str})`)();
 
   const resetInput = () => {
     setText("");
@@ -29,37 +37,188 @@ const App = () => {
   };
 
   const buttonColor = "#f2a33c";
+
+  const row_one = [
+    {
+      symbol: "Clear",
+      color: null,
+      handleClick: resetInput,
+      isLarge: true,
+    },
+    {
+      symbol: "C",
+      color: null,
+      handleClick: backspace,
+      isLarge: false,
+    },
+    {
+      symbol: "/",
+      color: buttonColor,
+      handleClick: addToText,
+      isLarge: false,
+    },
+  ];
+
+  const row_two = [
+    {
+      symbol: "7",
+      color: null,
+      handleClick: addToText,
+      isLarge: true,
+    },
+    {
+      symbol: "8",
+      color: null,
+      handleClick: addToText,
+      isLarge: false,
+    },
+    {
+      symbol: "9",
+      color: null,
+      handleClick: addToText,
+      isLarge: false,
+    },
+    {
+      symbol: "-",
+      color: buttonColor,
+      handleClick: addToText,
+      isLarge: false,
+    },
+  ];
+
+  const row_three = [
+    {
+      symbol: "4",
+      color: null,
+      handleClick: addToText,
+      isLarge: true,
+    },
+    {
+      symbol: "5",
+      color: null,
+      handleClick: addToText,
+      isLarge: false,
+    },
+    {
+      symbol: "6",
+      color: null,
+      handleClick: addToText,
+      isLarge: false,
+    },
+    {
+      symbol: "*",
+      color: buttonColor,
+      handleClick: addToText,
+      isLarge: false,
+    },
+  ];
+
+  const row_four = [
+    {
+      symbol: "1",
+      color: null,
+      handleClick: addToText,
+      isLarge: true,
+    },
+    {
+      symbol: "2",
+      color: null,
+      handleClick: addToText,
+      isLarge: false,
+    },
+    {
+      symbol: "3",
+      color: null,
+      handleClick: addToText,
+      isLarge: false,
+    },
+    {
+      symbol: "+",
+      color: buttonColor,
+      handleClick: addToText,
+      isLarge: false,
+    },
+  ];
+
+  const row_five = [
+    {
+      symbol: "0",
+      color: null,
+      handleClick: addToText,
+      isLarge: true,
+    },
+    {
+      symbol: ".",
+      color: null,
+      handleClick: addToText,
+      isLarge: false,
+    },
+    {
+      symbol: "=",
+      color: null,
+      handleClick: calculateResult,
+      isLarge: false,
+    },
+  ];
+
   return (
     <div className="container">
       <div className="wrapper">
         <Display text={text} result={result} />
         <div className="row">
-          <Button symbol="7" handleClick={addToText} />
-          <Button symbol="8" handleClick={addToText} />
-          <Button symbol="9" handleClick={addToText} />
-          <Button symbol="/" color={buttonColor} handleClick={addToText} />
+          {row_one.map((el) => (
+            <Button
+              key={el.symbol}
+              symbol={el.symbol}
+              handleClick={el.handleClick}
+              isLarge={el.isLarge}
+              color={el.color}
+            />
+          ))}
         </div>
         <div className="row">
-          <Button symbol="4" handleClick={addToText} />
-          <Button symbol="5" handleClick={addToText} />
-          <Button symbol="6" handleClick={addToText} />
-          <Button symbol="*" color={buttonColor} handleClick={addToText} />
+          {row_two.map((el) => (
+            <Button
+              key={el.symbol}
+              symbol={el.symbol}
+              handleClick={el.handleClick}
+              isLarge={el.isLarge}
+              color={el.color}
+            />
+          ))}
         </div>
         <div className="row">
-          <Button symbol="1" handleClick={addToText} />
-          <Button symbol="2" handleClick={addToText} />
-          <Button symbol="3" handleClick={addToText} />
-          <Button symbol="+" color={buttonColor} handleClick={addToText} />
+          {row_three.map((el) => (
+            <Button
+              key={el.symbol}
+              symbol={el.symbol}
+              handleClick={el.handleClick}
+              isLarge={el.isLarge}
+              color={el.color}
+            />
+          ))}
         </div>
         <div className="row">
-          <Button symbol="0" handleClick={addToText} />
-          <Button symbol="." handleClick={addToText} />
-          <Button symbol="=" handleClick={calculateResult} />
-          <Button symbol="-" color={buttonColor} handleClick={addToText} />
+          {row_four.map((el) => (
+            <Button
+              key={el.symbol}
+              symbol={el.symbol}
+              handleClick={el.handleClick}
+              isLarge={el.isLarge}
+              color={el.color}
+            />
+          ))}
         </div>
         <div className="row">
-          <Button symbol="Clear" handleClick={resetInput} />
-          <Button symbol="C" handleClick={backspace} />
+          {row_five.map((el) => (
+            <Button
+              key={el.symbol}
+              symbol={el.symbol}
+              handleClick={el.handleClick}
+              isLarge={el.isLarge}
+              color={el.color}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import "./Button.scss";
 
-const Button = ({ symbol, color, handleClick }) => {
+const Button = ({ symbol, color, handleClick, isLarge }) => {
   return (
     <div
       onClick={() => handleClick(symbol)}
-      className="button-wrapper"
+      className={`button-wrapper ${isLarge ? "large-button" : ""}`}
       style={{ backgroundColor: color }}
     >
       {symbol}
